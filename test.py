93efcda61e9da25091187e6ff94a8cbf8e4c659f
changho <jackshin0807@gmail.com>
2024-01-31 16:39:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,44 @@ print(a)
 
 b = 30
 print(b)
+
+print('hello world')
+
+#엄청나게 빠르게 성장하기 위한 데브코드를 작성하자
+# git을 자유자재로 사용하기 위해 진심으로 노력하자
+
+
+name  = "mike"
+
+print(name)
+
+city = 'seoul'
+print(city)
+
+
+#리스트 
+names = ['mike','jane','tom']
+print(names)
+
+
+#리스트에 있는 값을 불러오기 위해서는 index를 활용한다
+print(names[0])
+print(names[1])
+print(names[2])
+
+#이미 for문 안에 names 라는 리스트를 지정해 주었으니
+# print단계에서 names를 다시 한번 지정하지 않는다.
+for i in names:
+    print(i)
+
+#딕셔너리
+#이걸로 묶어서 DB로 활용할 수 있을 것이다
+#기본 개념들 가볍게 여기지 말고 집중해서 잘 해보자
+user ={}
+user['age'] = 25
+user['address'] = 'seoul'
+
+print(user)
+
+#왜 업로드가 안되는 것일까?
+
